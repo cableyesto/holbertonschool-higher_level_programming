abe9c036a314b6aee39e4a4d8f3cc4389938ad29
cableyesto <dialkins@tutamail.com>
2025-07-28 13:41:50 +0200

    Project 1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Documentation of the square module"""
+
+
+class Square:
+    """Square class with size private attribute"""
+    def __init__(self, new_size):
+        self.__size = new_size
