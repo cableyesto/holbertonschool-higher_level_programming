f06e29ccf54f44134059081466a4898ed3618502
cableyesto <dialkins@tutamail.com>
2025-07-28 14:03:09 +0200

    Project 2. Size validation.

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Documentation of the square module"""
+
+
+class Square:
+    """Square class with size private attribute, possibility to """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
