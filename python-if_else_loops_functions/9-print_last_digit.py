a73bac05d024afaa981d34b6cee70ea23dd33f0f
cableyesto <dialkins@tutamail.com>
2025-07-28 10:36:25 +0200

    Take the -98 test case modification

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = number % 10
-    print(last_digit, end='')
+    print("{}".format(last_digit), end='')
     if number < 0:
-        last_digit = (-1) * number % 10
-        print(last_digit, end='')
+        last_digit = abs(number) % 10
+        print("{}".format(last_digit), end='')
     return last_digit
