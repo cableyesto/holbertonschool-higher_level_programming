a1fd07371c54d26062ecbb6b07cbe5099967063f
cableyesto <dialkins@tutamail.com>
2025-07-30 11:56:32 +0200

    Start the module JavaScript - Web scraping, project 0. Readme.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+var args = process.argv.slice(2);
+
+try {
+  const data = fs.readFileSync(args[0], 'utf8');
+  console.log('File content:', data);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
