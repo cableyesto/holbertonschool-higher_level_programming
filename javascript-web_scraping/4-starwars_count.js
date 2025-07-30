d6f392f05d5a1c044b8e96b2ed7deb087dd63a06
cableyesto <dialkins@tutamail.com>
2025-07-30 18:11:45 +0200

    Fix semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -11,9 +11,9 @@ try {
   request(options, function (error, response, body) {
     if (!error) {
       const bodyJSON = JSON.parse(body);
-      const results = bodyJSON.results; 
+      const results = bodyJSON.results;
 
-      const filteredResults = results.filter(movie => {  
+      const filteredResults = results.filter(movie => {
         const subStr = '18';
         const subArr = movie.characters.filter(str => str.includes(subStr));
         if (subArr.length > 0) {
