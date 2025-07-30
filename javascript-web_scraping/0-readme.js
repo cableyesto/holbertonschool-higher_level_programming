aeecf082a19de9ecfa09962caec6b4036ab70770
cableyesto <dialkins@tutamail.com>
2025-07-30 12:01:00 +0200

    Change what is printed.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ let args = process.argv.slice(2);
 
 try {
   const data = fs.readFileSync(args[0], 'utf8');
-  console.log('File content:', data);
+  console.log(data);
 } catch (err) {
   console.error('Error reading file:', err);
 }
