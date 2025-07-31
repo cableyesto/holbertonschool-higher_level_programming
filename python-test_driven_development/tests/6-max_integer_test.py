7d42daee335e1591e5c333e34545ec4a7a7c1eb6
cableyesto <dialkins@tutamail.com>
2025-07-31 17:07:38 +0200

    Add some tests to fit the requirements

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,5 +10,20 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_normal(self):
+    def test_one(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_max_beginning(self):
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+    def test_max_middle(self):
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+
+    def test_max_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer([-1, 3]), 3)
+
+    def test_only_negative(self):
+        self.assertEqual(max_integer([-1, -2]), -1)
